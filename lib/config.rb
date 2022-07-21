a52b6a14217abb2119a12adf13d0c6c1f848bfc9
Tracy Atteberry <tracy@magicbydesign.com>
2022-07-20 23:52:00 -0500

    Check for config path

--- lib/config.rb
+++ lib/config.rb
@@ -18,11 +18,14 @@ module MyCLI
 
     # public scope to facilitate testing
     def load_config(config_path)
-      @data = YAML.safe_load_file(
-        config_path,
-        aliases: true,
-        symbolize_names: true
-      )
+      @data = {}
+      if File.exist?(config_path)
+        @data = YAML.safe_load_file(
+          config_path,
+          aliases: true,
+          symbolize_names: true
+        )
+      end
     end
 
     def show
